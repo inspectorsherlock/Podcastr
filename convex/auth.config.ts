f23c7a44b479019d467b00b4657cad769b1c349f
Fawwaz <fawwaz.chowdhury@googlemail.com>
2024-11-13 20:05:44 +0000

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://exciting-perch-32.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
---
+++ types/openai.d.ts
@@ -0,0 +1,4 @@
+declare module "openai/resources/audio/speech.mjs" {
+  import { SpeechCreateParams } from "openai"; // Adjust as needed
+  export { SpeechCreateParams };
+}
